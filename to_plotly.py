11fb94314f97e357f6a00d425a3bc18180152ea2
Jerome Dockes <jerome@dockes.org>
2018-06-21 14:07:29 +0200

    add threshold + better cmap

--- to_plotly.py
+++ to_plotly.py
@@ -1,82 +1,106 @@
 import argparse
 import json
 
+import numpy as np
 from nilearn import surface, datasets
 
 HTML_TEMPLATE = """
 <html>
-    <head>
-        <title>surface plot</title>
-        <meta charset="UTF-8" />
-        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script>
 
-function addPlot() {
+<head>
+    <title>surface plot</title>
+    <meta charset="UTF-8" />
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    <script
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
+    <script>
+        function addPlot() {
 
             let surface_map_info = INSERT_STAT_MAP_JSON_HERE;
-let hemisphere = $("#select-hemisphere").val();
-let kind = $("#select-kind").val();
-            makePlot(surface_map_info[kind + "_" + hemisphere], "surface-plot");
-}
-
-function makePlot(info, divId){
-
-    info["type"] = "mesh3d";
-    info["colorscale"] = [
-        [0, 'rgb(0, 0, 255)'],
-        [0.2, 'rgb(20, 0, 230)'],
-        [0, 'rgb(20, 0, 230)'],
-        [1, 'rgb(255, 0, 0)']
-    ];
-
-    let data = [ info ];
-    let axisConfig = {showgrid: false,
-                      showline: false, ticks: '',
-                      showticklabels: false,
-                      zeroline: false};
-    let layout = {
-        width: 1000,
-        height: 1000,
-        scene: {
-            xaxis: axisConfig,
-            yaxis: axisConfig,
-            zaxis: axisConfig
+            let hemisphere = $("#select-hemisphere").val();
+            let kind = $("#select-kind").val();
+            makePlot(
+                surface_map_info[kind + "_" + hemisphere], "surface-plot");
         }
-    };
 
-    Plotly.react(divId, data, layout);
-}
+        function makePlot(info, divId) {
 
-        </script>
-        <script>
-$(document).ready(
-function() {
-addPlot();
-$("#select-hemisphere").change(addPlot)
-$("#select-kind").change(addPlot)
+            info["type"] = "mesh3d";
 
-});
+            info["colorscale"] = [
+                [0.0, "rgb(255, 255, 255)"],
+                [0.111, "rgb(34, 255, 255)"],
+                [0.222, "rgb(0, 131, 255)"],
+                [0.333, "rgb(0, 0, 233)"],
+                [0.444, "rgb(0, 0, 86)"],
+                [0.556, "rgb(86, 0, 0)"],
+                [0.667, "rgb(233, 0, 0)"],
+                [0.778, "rgb(255, 131, 0)"],
+                [0.889, "rgb(255, 255, 34)"],
+                [1.0, "rgb(255, 255, 255)"]
+            ];
 
-</script>
-    </head>
-    <body>
-        <div id="surface-plot"></div>
-<select id="select-hemisphere">
+            let data = [info];
+            let axisConfig = {
+                showgrid: false,
+                showline: false,
+                ticks: '',
+                showticklabels: false,
+                zeroline: false
+            };
+            let layout = {
+                width: 800,
+                height: 800,
+                scene: {
+                    xaxis: axisConfig,
+                    yaxis: axisConfig,
+                    zaxis: axisConfig
+                }
+            };
+
+            Plotly.react(divId, data, layout);
+        }
+    </script>
+    <script>
+        $(document).ready(
+            function() {
+                addPlot();
+                $("#select-hemisphere").change(addPlot)
+                $("#select-kind").change(addPlot)
+
+            });
+    </script>
+</head>
+
+<body>
+    <div id="surface-plot"></div>
+    <select id="select-hemisphere">
 <option value="left">Left hemisphere</option>
 <option value="right">Right hemisphere</option>
 </select>
 
-<select id="select-kind">
+    <select id="select-kind">
 <option value="inflated">Inflated</option>
 <option value="pial">Pial</option>
 </select>
 
-    </body>
+</body>
+
 </html>
 
 
 """
+
+
+def colorscale(cm):
+    x = np.linspace(0, 1, 10)
+    rgb = cm(x, bytes=True)[:, :3]
+    rgb = np.array(rgb, dtype=int)
+    colors = []
+    for i, col in zip(x, rgb):
+        colors.append([np.round(i, 3), "rgb({}, {}, {})".format(*col)])
+    return json.dumps(colors)
 
 
 def to_plotly(mesh, stat_map, out_file=None):
@@ -101,12 +125,15 @@ def to_plotly(mesh, stat_map, out_file=None):
     return info
 
 
-def full_brain_info(stat_map):
+def full_brain_info(stat_map, threshold=None):
     info = {}
     fsaverage = datasets.fetch_surf_fsaverage5()
     for hemi in ['left', 'right']:
         pial = fsaverage['pial_{}'.format(hemi)]
         surf_map = surface.vol_to_surf(stat_map, pial)
+        if threshold is not None:
+            abs_threshold = np.percentile(np.abs(surf_map), threshold)
+            surf_map[np.abs(surf_map) < abs_threshold] = np.nan
         info['pial_{}'.format(hemi)] = to_plotly(pial, surf_map)
         info['inflated_{}'.format(hemi)] = to_plotly(
             fsaverage['infl_{}'.format(hemi)], surf_map)
@@ -116,10 +143,11 @@ def full_brain_info(stat_map):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('stat_map', type=str)
+    parser.add_argument('--threshold', type=int, default=None)
     parser.add_argument(
         '--out_file', type=str, default='surface_plot_standalone.html')
     args = parser.parse_args()
-    as_json = json.dumps(full_brain_info(args.stat_map))
+    as_json = json.dumps(full_brain_info(args.stat_map, args.threshold))
     as_html = HTML_TEMPLATE.replace('INSERT_STAT_MAP_JSON_HERE', as_json)
     with open(args.out_file, 'w') as f:
         f.write(as_html)
