542729914d95a88f436ebce75870e4b4a0a8cf73
Jerome Dockes <jerome@dockes.org>
2018-06-21 13:34:56 +0200

    add selection of surfaces

--- to_plotly.py
+++ to_plotly.py
@@ -3,6 +3,81 @@ import json
 
 from nilearn import surface, datasets
 
+HTML_TEMPLATE = """
+<html>
+    <head>
+        <title>surface plot</title>
+        <meta charset="UTF-8" />
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script>
+
+function addPlot() {
+
+            let surface_map_info = INSERT_STAT_MAP_JSON_HERE;
+let hemisphere = $("#select-hemisphere").val();
+let kind = $("#select-kind").val();
+            makePlot(surface_map_info[kind + "_" + hemisphere], "surface-plot");
+}
+
+function makePlot(info, divId){
+
+    info["type"] = "mesh3d";
+    info["colorscale"] = [
+        [0, 'rgb(0, 0, 255)'],
+        [0.2, 'rgb(20, 0, 230)'],
+        [0, 'rgb(20, 0, 230)'],
+        [1, 'rgb(255, 0, 0)']
+    ];
+
+    let data = [ info ];
+    let axisConfig = {showgrid: false,
+                      showline: false, ticks: '',
+                      showticklabels: false,
+                      zeroline: false};
+    let layout = {
+        width: 1000,
+        height: 1000,
+        scene: {
+            xaxis: axisConfig,
+            yaxis: axisConfig,
+            zaxis: axisConfig
+        }
+    };
+
+    Plotly.react(divId, data, layout);
+}
+
+        </script>
+        <script>
+$(document).ready(
+function() {
+addPlot();
+$("#select-hemisphere").change(addPlot)
+$("#select-kind").change(addPlot)
+
+});
+
+</script>
+    </head>
+    <body>
+        <div id="surface-plot"></div>
+<select id="select-hemisphere">
+<option value="left">Left hemisphere</option>
+<option value="right">Right hemisphere</option>
+</select>
+
+<select id="select-kind">
+<option value="inflated">Inflated</option>
+<option value="pial">Pial</option>
+</select>
+
+    </body>
+</html>
+
+
+"""
+
 
 def to_plotly(mesh, stat_map, out_file=None):
     mesh = surface.load_surf_mesh(mesh)
@@ -26,12 +101,25 @@ def to_plotly(mesh, stat_map, out_file=None):
     return info
 
 
+def full_brain_info(stat_map):
+    info = {}
+    fsaverage = datasets.fetch_surf_fsaverage5()
+    for hemi in ['left', 'right']:
+        pial = fsaverage['pial_{}'.format(hemi)]
+        surf_map = surface.vol_to_surf(stat_map, pial)
+        info['pial_{}'.format(hemi)] = to_plotly(pial, surf_map)
+        info['inflated_{}'.format(hemi)] = to_plotly(
+            fsaverage['infl_{}'.format(hemi)], surf_map)
+    return info
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('stat_map', type=str)
+    parser.add_argument(
+        '--out_file', type=str, default='surface_plot_standalone.html')
     args = parser.parse_args()
-
-    fsaverage = datasets.fetch_surf_fsaverage5()
-    mesh = fsaverage['pial_left']
-    surf_map = surface.vol_to_surf(args.stat_map, mesh)
-    as_json = to_plotly(mesh, surf_map, 'info.json')
+    as_json = json.dumps(full_brain_info(args.stat_map))
+    as_html = HTML_TEMPLATE.replace('INSERT_STAT_MAP_JSON_HERE', as_json)
+    with open(args.out_file, 'w') as f:
+        f.write(as_html)
